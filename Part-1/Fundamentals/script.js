bfe9f82077a034ae4e3a26fe2c11adf4364fd957
Parva Shastri <shastriparva@gmail.com>
2024-09-25 22:00:47 +0530

    Learned till topic 2.3, not much code added here

--- Part-1/Fundamentals/script.js
+++ Part-1/Fundamentals/script.js
@@ -1,3 +1,11 @@
 // Task - 1
 // 1.2
-// alert("I'm Javascript");
+// alert("I'm second Javascript");
+
+/* // Use strict for modern JS:
+"use strict";
+
+alert("some code");
+// "use strict" below is ignored--it must be at the top
+
+// strict mode is not activated */
