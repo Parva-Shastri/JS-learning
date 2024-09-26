71dc42307a803c025b1c0a5f42dae6fbd9652aa1
Parva Shastri <shastriparva@gmail.com>
2024-09-26 22:57:15 +0530

    2.4 Variable tasks

--- Part-1/Fundamentals/script.js
+++ Part-1/Fundamentals/script.js
@@ -9,3 +9,31 @@ alert("some code");
 // "use strict" below is ignored--it must be at the top
 
 // strict mode is not activated */
+
+// Variables task: from 2.4
+
+    // Task 1
+        // Declare two variables: admin and name.
+        // Assign the value "John" to name.
+        // Copy the value from name to admin.
+        // Show the value of admin using alert (must output “John”).
+
+    /* let admin
+    let name = "John";
+
+    admin = name;
+
+    alert( admin );
+    alert( name ); */
+
+    // Task 2
+        // Create a variable with the name of our planet. How would you name such a variable?
+        // Create a variable to store the name of a current visitor to a website. How would you name that variable?
+        
+        /* // My answer
+            let PLANET_EARTH;
+            let currentUser;
+            
+        // Correct Solution
+            let ourPlanetName = "Earth";
+            let currentUserName = "John"; */
