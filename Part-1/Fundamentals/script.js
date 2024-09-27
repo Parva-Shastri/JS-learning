e98e523f327773ad5e644d4d769f276049334cd6
Parva Shastri <shastriparva@gmail.com>
2024-09-27 22:21:57 +0530

    Completed topics: 2.5 Data types 2.6 Interactions
    Completed Part- 1 of #1

--- Part-1/Fundamentals/script.js
+++ Part-1/Fundamentals/script.js
@@ -13,12 +13,13 @@ alert("some code");
 // Variables task: from 2.4
 
     // Task 1
+    /* 
         // Declare two variables: admin and name.
         // Assign the value "John" to name.
         // Copy the value from name to admin.
         // Show the value of admin using alert (must output “John”).
 
-    /* let admin
+    let admin
     let name = "John";
 
     admin = name;
@@ -27,13 +28,62 @@ alert("some code");
     alert( name ); */
 
     // Task 2
+    /*
         // Create a variable with the name of our planet. How would you name such a variable?
         // Create a variable to store the name of a current visitor to a website. How would you name that variable?
         
-        /* // My answer
+        // My answer
             let PLANET_EARTH;
             let currentUser;
             
         // Correct Solution
             let ourPlanetName = "Earth";
             let currentUserName = "John"; */
+
+// Data types
+
+    /* 
+
+    // `typeof` is a operator, not a function!
+
+    typeof undefined // "undefined"
+
+    typeof 0 // "number"
+
+    typeof 10n // "bigint"
+
+    typeof true // "boolean"
+
+    typeof "foo" // "string"
+
+    typeof Symbol("id") // "symbol"
+
+    typeof Math // "object"
+
+    typeof null // "object" // `null` is not a object, this is an error of `typeof`
+
+    typeof alert // "function" // function is not a data type in JS, another error by `typeof`
+    */
+    /*
+    // Practice tasks
+    let name = "Parva";
+
+    let num1 = 11;
+    let num2 = 8;
+
+    alert( `hello, I am ` + (num1 + num2) + " years old!");
+
+    alert( `hello ${'name'}` );
+
+    alert( `hello ${name}` );
+    */
+
+// Interaction: alert, prompt, confirm
+
+    // Task
+    /*
+    let name = prompt("What is your name?", "John");
+
+    confirm(`Your name is ${name}`);
+    */
+
